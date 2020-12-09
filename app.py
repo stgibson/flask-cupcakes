31289af56d6625e452404916ba329b5584ecead1
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-09 15:19:14 -0800

    Created Cupcake model and skeleton code for app. Modified models.py and app.py.

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-"""Flask app for Cupcakes"""
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "kubrick"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
--- models.py
+++ models.py
@@ -1,1 +1,38 @@
-"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """
+        Connects app to the SQL database
+        type app: Flask
+    """
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """
+        Defines cupcakes table in db with an id, a flavor, a size, a rating,
+        and an image
+    """
+    __tablename__ = "cupcakes"
+
+    default_image = "https://tinyurl.com/demo-cupcake"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    flavor = db.Column(db.Text, nullable=False)
+
+    size = db.Column(db.Text, nullable=False)
+
+    rating = db.Column(db.Float, nullable=False)
+
+    image = db.Column(db.Text, nullable=False, default=default_image)
+
+    def __repr__():
+        """
+            For showing data in an instance
+            rtype: str
+        """
+        return f"<Cupcake id={id} flavor={flavor} size={size} rating={rating} \
+            image={image}"
